6c070f87430a449b6b67d36c2682fd72fe8e61fd
smallblack <653014496@qq.com>
2018-01-14 23:57:53 +0800

    添加ColorSelector

--- Journal/MainModel.cs
+++ Journal/MainModel.cs
@@ -13,6 +13,9 @@ namespace Journal
         public FontFamily CurrentFont { get { return _CurrentFont; } set { if (_CurrentFont == value) return; _CurrentFont = value; OnPropertyChanged(nameof(CurrentFont)); } }
         private FontFamily _CurrentFont = new FontFamily("Comic Sans MS");
 
+        public Brush CurrentColor { get { return _CurrentColor; } set { if (_CurrentColor == value) return; _CurrentColor = value; OnPropertyChanged(nameof(CurrentColor)); } }
+        private Brush _CurrentColor = Brushes.Black;
+
         private void OnPropertyChanged(string aPropertyName)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(aPropertyName));
